4f367d1c8dbb65dd3846ee24c096c49fdfc25701
Krzysztof Urban <urbankrzysztof92@gmail.com>
2020-09-08 22:13:11 +0200

    Project init

---
+++ src/main/java/com/example/gateway/Application.java
@@ -0,0 +1,44 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableConfigurationProperties(UriConfiguration.class)
+@RestController
+class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri("http://httpbin.org:80"))
+                .build();
+    }
+}
+
+@ConfigurationProperties
+class UriConfiguration {
+
+    private String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
+}
